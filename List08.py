0f67c8ed4bf553deb95029dffa643391b23c7886
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-04 17:20:42 +0500

    8m

--- List08.py
+++ List08.py
@@ -1,4 +1,27 @@
 def main(fruits):
+    a=fruits.count('apple')
+    if a==1:
+        fruits.remove('apple')
+    elif a==2:
+        fruits.remove('apple')
+        fruits.remove('apple')
+    elif a==3:
+        fruits.remove('apple')
+        fruits.remove('apple')
+        fruits.remove('apple')
+    elif a==4:
+        fruits.remove('apple')
+        fruits.remove('apple')
+        fruits.remove('apple')
+        fruits.remove('apple')
+        
+    elif a==5:
+        fruits.remove('apple')
+        fruits.remove('apple')
+        fruits.remove('apple')
+        fruits.remove('apple')
+        fruits.remove('apple')
+    return fruits
     """
     A list called "fruits" is given and is five in length and contains at least one "apple". Remove the apples from the list and return the list.
     Args:
@@ -6,4 +29,4 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    
