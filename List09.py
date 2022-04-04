0110fc35f2d56541254c81c8cd43b0494f58f160
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-04 17:50:24 +0500

    9m

--- List09.py
+++ List09.py
@@ -1,4 +1,28 @@
 def main(fruits):
+    bb=[]
+    a=fruits.count('apple')
+    bb.append(a)
+    if a==1:
+        bb.append(fruits.index('apple'))
+    elif a==2:
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+    elif a==3:
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+    elif a==4:
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        
+    elif a==5:
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
+        bb.append(fruits.index('apple'))
     """
     A list called “fruits” is given and is five in length and contains at least one “apple”. Calculate how many “apple” were involved and return the indexes in a list view.
     Args:
@@ -6,4 +30,4 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    return  bb
