3b4e5c9ff65380a3f9b91348b54a0a0f8caf9a70
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-04 15:32:03 +0500

    6m

--- List06.py
+++ List06.py
@@ -1,5 +1,5 @@
 def main(fruits):
-    a=fruits.count("olma")
+    fruits.count("olma")
     """
     Given a list called Fruits, it contains at least one apple. Find how many apples are on the list and return.
     Args:
@@ -7,4 +7,4 @@ def main(fruits):
     Returns:
         int: return answer
     """
-    return a
+    return fruits
