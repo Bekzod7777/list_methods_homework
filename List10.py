717341ae61baa7c31711c19dde6b3e648528f0d8
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-06 15:43:29 +0500

    10

--- List10.py
+++ List10.py
@@ -1,4 +1,13 @@
+from re import S
+from this import s
+
+
 def main(list1):
+    s=[]
+    bir=list1.count(1)
+    nol=list1.count(0)
+    s.append(bir)
+    s.append(nol)
     """
     A list of zeros and ones is given. Find how many ones and how many zeros there are and return to the list form.
     Args:
@@ -6,4 +15,7 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    return s
+list1=[1,0,1,0,0,0,0,1]
+print(main(list1))
+    
